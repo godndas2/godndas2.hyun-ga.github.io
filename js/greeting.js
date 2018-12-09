57e24eb3bca4a63f6a744fd554bf86b7f318275b
hyun <34512538+godndas2@users.noreply.github.com>
2018-12-09 17:59:34 +0900

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,46 +3,49 @@
  * greeting.js
  * */
 
+
 const nameContainer = document.querySelector(".js-name");
 
-function welcomeName(name) {
-	nameContainer.innerHTML = "";
-	const title = document.createElement("span");
-	title.className = "name__text";
-	title.innerHTML = `Hello ${name}`;
-	nameContainer.appendChild(title);
-} // 메인 화면에 나타나는 Hello (이름)
-
-function handleSubmit(event) {
-	event.preventDefault();
-	const form = event.target;
-	const input = form.querySelector("input");
-	const value = input.value;
-	localStorage.setItem("username", value);
-	welcomeName(name);
+function paintName(name) {
+  nameContainer.innerHTML = "";
+  const title = document.createElement("span");
+  title.className = "name__text";
+  title.innerHTML = `Hello ${name}`;
+  nameContainer.appendChild(title);
 }
 
-function nameInput() {
-	const input = document.createElement("input");
-	input.placeholder = "Type Your name here";
-	input.type = "text";
-	input.className = "name__input";
-	const form = document.createElement("form");
-	form.addEventListener("submit", handleSubmit);
-	form.appendChild(input);
-	nameContainer.appendChild(form);
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const input = form.querySelector("input");
+  const value = input.value;
+  localStorage.setItem("username", value);
+  paintName(value);
+}
+
+function paintInput() {
+  const input = document.createElement("input");
+  input.placeholder = "Type your name here";
+  input.type = "text";
+  input.className = "name__input";
+  const form = document.createElement("form");
+  form.addEventListener("submit", handleSubmit);
+  form.appendChild(input);
+  nameContainer.appendChild(form);
 }
 
 function loadName() {
-	  const name = localStorage.getItem("username");
-	  if (name === null) {
-		  nameInput();
-	  } else {
-		  welcomeName(name);
-	  }
-	}
+  const name = localStorage.getItem("username");
+  if (name === null) {
+    paintInput();
+  } else {
+    paintName(name);
+  }
+}
 
 function init() {
-	  loadName();
-	}
+  loadName();
+}
+
 init();
+
